a0f6e225cc31bfd0863a8b23ad951e444cd975b9
kristynf <thetruthandsoul@gmail.com>
2020-01-23 21:49:53 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,57 +10,31 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
 
-//This program with print the months by how many days they last
+//This program with print the months by how many days they contain.
 
-    { System.out.println("How many days do you want to the months displayed to contain?: ");
+    { System.out.println("How many days do you want the months displayed to contain?: ");
      int  answer = sc.nextInt();
 
-        {if answer()
 
-    int month = 5;
+
+    int month = (answer);
     String monthName;
                 switch(month)
 
     {
-        case 1:
-            monthName = "January";
+        case 31:
+            monthName = "January, March, May, July, August, October, and December";
             break;
-        case 2:
+        case 30:
+            monthName = "April, June, September, November";
+            break;
+        case 28:
             monthName = "February";
             break;
-        case 3:
-            monthName = "March";
-            break;
-        case 4:
-            monthName = "April";
-            break;
-        case 5:
-            monthName = "May";
-            break;
-        case 6:
-            monthName = "June";
-            break;
-        case 7:
-            monthName = "July";
-            break;
-        case 8:
-            monthName = "August";
-            break;
-        case 9:
-            monthName = "September";
-            break;
-        case 10:
-            monthName = "October";
-            break;
-        case 11:
-            monthName = "November";
-            break;
-        case 12:
-            monthName = "December";
-            break;
-        default:
+        case 0:
             monthName = "Unknown";
             break;
+
     }
         System.out.println(monthName);
 }
